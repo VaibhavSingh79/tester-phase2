faa58b71637612785061b2c2cd437fe83033fbe8
sbx_user1051 <sbx_user1051@169.254.28.109>
2026-04-29 12:14:53 +0000

    feat: auto-generated infrastructure from Excel POC

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,6 +2,6 @@
 provider "aws" {
   region = "us-west-2"
   assume_role {
-    role_arn = "arn:aws:iam::676206905121:role/TerraformDeployRole"
+    role_arn = "arn:aws:iam::202533543944:role/TerraformDeployRole"
   }
 }
