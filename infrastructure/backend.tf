3e25209c7105e2d5fd1a7c92fbe11b03891fd403
sbx_user1051 <sbx_user1051@169.254.47.39>
2026-07-29 06:13:57 +0000

    feat: auto-generated infrastructure from Excel

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "996122083346-infraorchestrator-tfstate"
     key     = "VaibhavSingh79-tester-phase2/terraform.tfstate"
-    region  = "us-west-2"
+    region  = "ap-south-1"
     encrypt = true
     dynamodb_table = "infraorchestrator-tf-locks"
   }
--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -7,7 +7,7 @@ provider "aws" {
   default_tags {
     tags = {
       Requester = "81135dfa-0021-70de-c8af-d63caf7885ba"
-      JobId = "f05b7577-2c5c-45d9-a473-4d25a1dd03d9"
+      JobId = "a111079f-b172-47a9-9db4-f269397c7451"
       ManagedBy = "InfraOrchestrator"
     }
   }
