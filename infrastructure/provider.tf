e0ddc88a7cce13bd0abb8d805a8f3f59ff9608b0
sbx_user1051 <sbx_user1051@169.254.16.65>
2026-07-31 10:31:52 +0000

    feat: auto-generated infrastructure from Excel

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -7,7 +7,7 @@ provider "aws" {
   default_tags {
     tags = {
       Requester = "81135dfa-0021-70de-c8af-d63caf7885ba"
-      JobId = "dd8096d5-bb75-42da-9c45-153dedcae080"
+      JobId = "6cf6d932-0daf-4597-b1b3-181ba8f9d1a7"
       ManagedBy = "InfraOrchestrator"
     }
   }
